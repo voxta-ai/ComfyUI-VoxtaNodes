298ddbdb2fa204e8d67958cc5c1bae52dfa4ef39
Acid Bubbles <acidbubbles.anon@gmail.com>
2025-10-06 15:18:06 -0400

    Voxta Nodes project scaffolding

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for voxta."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """Team Voxta"""
+__email__ = "info@voxta.ai"
+__version__ = "0.0.1"
+
+from .src.voxta.nodes import NODE_CLASS_MAPPINGS
+from .src.voxta.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
